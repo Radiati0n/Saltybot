ece22993e3e067c40cb989f4e48c949e5b98b708
Radiati0n <Nicko01@comcast.net>
2015-11-04 20:44:26 -0500

    Updated cookie
    Updated PHPSESSID and __cfduid

--- SaltyBot.py
+++ SaltyBot.py
@@ -4,6 +4,6 @@ class SaltyBot:
     def __init__(self):
         self.opener = urllib2.build_opener()
         self.opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36')]
-        self.opener.addheaders.append(('Cookie', 'PHPSESSID=rnkfrr4afis2e13runogvcl236'))
-        self.opener.addheaders.append(('Cookie', '__cfduid=d9074a47ba86078971f5e74c9398739f71432266025'))
+        self.opener.addheaders.append(('Cookie', 'PHPSESSID=4ik496l8404976f0m98q63go84'))
+        self.opener.addheaders.append(('Cookie', '__cfduid=d5866a182ac994d0d7fe5268086e20e2e1446683623'))
         self.opener.addheaders.append(('Cookie', '_ga=GA1.2.81538886.1432266024'))
