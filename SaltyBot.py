768619882029ce2c58f1b324b488c8fb66574604
TylerHolcombe <thulk14@aol.com>
2015-11-04 20:17:00 -0500

    Made initial file and init function
    Created file for class SaltyBot with an init function

---
+++ SaltyBot.py
@@ -0,0 +1,10 @@
+import urllib2
+
+class SaltyBot:
+
+    def __init__():
+        opener = urllib2.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36')]
+        opener.addheaders.append(('Cookie', 'PHPSESSID=rnkfrr4afis2e13runogvcl236'))
+        opener.addheaders.append(('Cookie', '__cfduid=d9074a47ba86078971f5e74c9398739f71432266025'))
+        opener.addheaders.append(('Cookie', '_ga=GA1.2.81538886.1432266024'))
