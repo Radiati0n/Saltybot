d1c35623baecc74a924cc6d806cb5ac09a4c3e01
Radiati0n <Nicko01@comcast.net>
2015-11-07 13:57:22 -0500

    Database connection
    Added dbConnect function which is run at initialization

--- SaltyBot.py
+++ SaltyBot.py
@@ -13,6 +13,7 @@ class SaltyBot:
         self.opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36')]
         self.opener.addheaders.append(('Cookie', 'PHPSESSID=phugqk1j1aqp210i0epcr5h6r6'))
         self.opener.addheaders.append(('Cookie', '__cfduid=d5866a182ac994d0d7fe5268086e20e2e1446683623'))
+        self.dbConnect()
         
     def getMatchData(self):
         mainPage = self.opener.open('http://www.saltybet.com/')
@@ -50,6 +51,9 @@ class SaltyBot:
         }
                          
         requests.post(url, data=params, headers=headers)
+
+    def dbConnect(self):
+        self.db = pymysql.connect(host='raspimumble.no-ip.org',port=3306,user='saltybot',passwd='salty-pass',db='saltydata')
                                                 
             
 bot = SaltyBot()
