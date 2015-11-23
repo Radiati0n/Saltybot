d0572b3d60c84e9c0fec063dff3a6499c037a1e8
Tyler Holcombe <TylerHolcombe@users.noreply.github.com>
2015-11-23 03:19:23 -0500

    Changed so that matchups and winners are still printed during exhibition mode, but no elo is used and no bets are made

--- SaltyBot-client.py
+++ SaltyBot-client.py
@@ -3,28 +3,29 @@ import time
 import sys
 
 bot = SaltyBot()
-exhibitionMessageShown = False
+print "SaltyBot initialized...\n"
 while True:
     try:
         bot.getMatchData()
 
-        if bot.gameStatus == "open" and bot.gameMode != "exhibition":
-            exhibitionMessageShown = False
+        if bot.gameStatus == "open":
             print "Money: " + bot.money
             print bot.playerOneName + " vs " + bot.playerTwoName
             
-            player1Elo = bot.getElo(0)
-            player2Elo = bot.getElo(1)
-            print bot.playerOneName + " Elo: " + str(player1Elo)
-            print bot.playerTwoName + " Elo: " + str(player2Elo)
+            if(bot.gameMode != "exhibition"):
+                player1Elo = bot.getElo(0)
+                player2Elo = bot.getElo(1)
+                print bot.playerOneName + " Elo: " + str(player1Elo)
+                print bot.playerTwoName + " Elo: " + str(player2Elo)
 
-            print "P(" + bot.playerOneName + " win): " + str(bot.eloWinProb(player1Elo, player2Elo))
-            print "P(" + bot.playerTwoName + " win): " + str(bot.eloWinProb(player2Elo, player1Elo))
-
-            if(bot.eloWinProb(player1Elo, player2Elo) > 0.5):
-                bot.bet(0, float(bot.money)*0.10)
+                print "P(" + bot.playerOneName + " win): " + str(bot.eloWinProb(player1Elo, player2Elo))
+                print "P(" + bot.playerTwoName + " win): " + str(bot.eloWinProb(player2Elo, player1Elo))
+                if(bot.eloWinProb(player1Elo, player2Elo) > 0.5):
+                    bot.bet(0, float(bot.money)*0.10)
+                else:
+                    bot.bet(1, float(bot.money)*0.10)
             else:
-                bot.bet(1, float(bot.money)*0.10)
+                print "No bets are made during exhibition mode"
 
             while bot.gameStatus == "open" or bot.gameStatus == "locked":
                 bot.getMatchData()
@@ -37,9 +38,6 @@ while True:
                 print bot.playerTwoName + " wins!"
                 s = 0
             print ""
-        elif bot.gameMode == "exhibition" and exhibitionMessageShown == False:
-            print "Not betting during exhibition mode"
-            exhibitionMessageShown = True
         time.sleep(5)
     except:
         e = sys.exc_info()[0]
